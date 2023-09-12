4fe97aa1773bec652cecb76c98ebecc4049cd754
Yarden-Tal <83368934+Yarden-Tal@users.noreply.github.com>
2023-09-12 22:15:54 +0300

    fix attempt 3

--- src/components/sections/projects/Projects.tsx
+++ src/components/sections/projects/Projects.tsx
@@ -1,26 +1,21 @@
 import React from "react";
-import { motion } from "framer-motion";
 import { SectionWrapper } from "../../../hoc";
 import { projects, projectsTxt } from "../../../data/data";
-import { fadeIn, textVariant } from "../../../utils/motion";
 import ProjectCard from "./ProjectCard";
 import { Project } from "../../../ts/interfaces";
 
 const Projects = (): JSX.Element => {
   return (
     <section>
-      <motion.div>
+      <div>
         <p className="sect-subtxt">My work</p>
         <h2 className="sect-head-txt">Projects.</h2>
-      </motion.div>
+      </div>
 
       <div className="w-full flex">
-        <motion.p
-          variants={fadeIn("", "", 0.1, 1)}
-          className="mt-3 text-white text-[17px] max-w-3xl leading-[30px]"
-        >
+        <p className="mt-3 text-white text-[17px] max-w-3xl leading-[30px]">
           {projectsTxt}
-        </motion.p>
+        </p>
       </div>
 
       <div className="mt-20 flex flex-wrap gap-7 justify-center">
