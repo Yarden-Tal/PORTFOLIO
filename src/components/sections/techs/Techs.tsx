b37de2ffa66ccb464a5acc15e7e92a93e4c0a40a
Yarden-Tal <83368934+Yarden-Tal@users.noreply.github.com>
2023-09-17 06:25:00 +0300

    bugfix: tech section test 1

--- src/components/sections/techs/Techs.tsx
+++ src/components/sections/techs/Techs.tsx
@@ -7,25 +7,29 @@ import { Technology } from "../../../ts/interfaces";
 
 const Techs = (): JSX.Element => {
   return (
-    <section className="flex flex-row flex-wrap justify-center gap-10">
+    <section className="flex flex-row flex-wrap justify-center gap-10 pad max-w-7xl mx-auto relative z-0">
       {technologies.map((t: Technology) => (
         <div
           data-tooltip-id={t.icon}
           data-tooltip-place="top"
           className="w-28 h-28"
-          key={t.name}>
-            <Tooltip 
-            className="change-color" 
-            id={t.icon} 
-            clickable   
-            data-tooltip-delay-hide={100}>
-              <a className="hover:text-tertiary" target="_blank" href={t.docsUrl}>{t.name}</a>
-            </Tooltip>
-            <BallCanvas icon={t.icon} />
+          key={t.name}
+        >
+          <Tooltip
+            className="change-color"
+            id={t.icon}
+            clickable
+            data-tooltip-delay-hide={100}
+          >
+            <a className="hover:text-tertiary" target="_blank" href={t.docsUrl}>
+              {t.name}
+            </a>
+          </Tooltip>
+          <BallCanvas icon={t.icon} />
         </div>
       ))}
     </section>
   );
 };
 
-export default SectionWrapper(Techs, "");
+export default Techs;
