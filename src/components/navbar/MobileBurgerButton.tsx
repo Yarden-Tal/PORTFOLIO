1fd91afab181c90364b5bae84e7d269c91382417
Yarden-Tal <83368934+Yarden-Tal@users.noreply.github.com>
2023-10-28 13:36:30 +0300

    amend for scroll state

--- src/components/navbar/MobileBurgerButton.tsx
+++ src/components/navbar/MobileBurgerButton.tsx
@@ -1,15 +1,30 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { menu, close } from "../../assets/assets";
 import MobileMenu from "./MobileMenu";
 import { useTheme } from "../../context/ColorThemeContext";
+import isScrolled from "../../utils/scroll";
 
 const MobileBurgerButton = ({ active, setActive }): JSX.Element => {
   const [toggle, setToggle] = useState<boolean>(false);
+  const [scrolled, setScrolled] = useState<boolean>(false);
+  const { isDarkMode } = useTheme();
+
+  useEffect(() => {
+    const checkScroll = (): void => {
+      isScrolled() ? setScrolled(true) : setScrolled(false);
+    };
+    window.addEventListener("scroll", checkScroll);
+    return () => window.removeEventListener("scroll", checkScroll);
+  }, []);
   return (
     <div className="sm:hidden">
       <div
         className={`flex flex-1 justify-end items-center p-2 rounded-full ${
-          useTheme().isDarkMode ? "bg-tertiary" : "bg-[#D4D4D4]"
+          isDarkMode
+            ? "bg-tertiary"
+            : scrolled
+            ? "bg-[#D4D4D4]"
+            : "bg-transparent"
         }`}
       >
         <img
