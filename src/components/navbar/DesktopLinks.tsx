bc33c799f1828821d2a58bb5ae11867c8fa92fa9
Yarden-Tal <83368934+Yarden-Tal@users.noreply.github.com>
2023-10-28 13:15:49 +0300

    import fix

--- src/components/navbar/DesktopLinks.tsx
+++ src/components/navbar/DesktopLinks.tsx
@@ -1,20 +1,28 @@
-import React from 'react'
-import { navLinks } from '../../data/data';
-import { NavLink } from '../../ts/types';
+import React from "react";
+import { navLinks } from "../../data/data";
+import { NavLink } from "../../ts/interfaces";
 
-const DesktopLinks = ({active, isDarkMode, setActive}): JSX.Element => {
-    return <ul className="list-none hidden sm:flex flex-row gap-10">
-    {navLinks.map((link: NavLink) => (
-      <li
-        key={link.id}
-        className={`${(active === link.title) ? "text-white" : (isDarkMode ? "text-secondary" : "text-primary")} 
+const DesktopLinks = ({ active, isDarkMode, setActive }): JSX.Element => {
+  return (
+    <ul className="list-none hidden sm:flex flex-row gap-10">
+      {navLinks.map((link: NavLink) => (
+        <li
+          key={link.id}
+          className={`${
+            active === link.title
+              ? "text-white"
+              : isDarkMode
+              ? "text-secondary"
+              : "text-primary"
+          } 
         hover:text-white text-[18px] font-medium cursor-pointer`}
-        onClick={() => setActive(link.title)}
-      >
-        <a href={`#${link.id}`}>{link.title}</a>
-      </li>
-    ))}
-  </ul>;
-}
+          onClick={() => setActive(link.title)}
+        >
+          <a href={`#${link.id}`}>{link.title}</a>
+        </li>
+      ))}
+    </ul>
+  );
+};
 
-export default DesktopLinks
+export default DesktopLinks;
